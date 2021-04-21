c104f06f44d7ed9796f8798c95a0f34a805bc2d9
jiatianqi <jiatianqi@baidu.com>
2021-04-21 17:08:34 +0800

    fix[阻止点击]：阻止页面点击造成前端路由切换造成页面变化的问题

--- js/inject.js
+++ js/inject.js
@@ -7,13 +7,18 @@ function preventReload(e) {
     var e = window.event || e;
     e.returnValue = '确定离开当前页面吗？';
 }
+function preventClick(e) {
+    e.stopPropagation();
+}
 
 function setPageEdit() {
     if (document.body.contentEditable == 'true') {
         window.removeEventListener('beforeunload', preventReload);
+        document.removeEventListener('click', preventClick, true);
         document.body.contentEditable = false;
     } else {
         window.addEventListener('beforeunload', preventReload);
+        document.addEventListener('click', preventClick, true);
         document.body.contentEditable = true;
     }
 }

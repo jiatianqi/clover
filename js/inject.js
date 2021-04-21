760d99b76a682d03fbd7a8c8f8661ee3d76ae43b
jiatianqi <jiatianqi@baidu.com>
2021-04-21 12:40:19 +0800

    feat[阻止跳转]：在页面编辑情况下，阻止页面自动跳转，需用户二次确认

--- js/inject.js
+++ js/inject.js
@@ -3,10 +3,17 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     sendResponse(document.body.contentEditable);
 });
 
+function preventReload(e) {
+    var e = window.event || e;
+    e.returnValue = '确定离开当前页面吗？';
+}
+
 function setPageEdit() {
     if (document.body.contentEditable == 'true') {
+        window.removeEventListener('beforeunload', preventReload);
         document.body.contentEditable = false;
     } else {
+        window.addEventListener('beforeunload', preventReload);
         document.body.contentEditable = true;
     }
 }
